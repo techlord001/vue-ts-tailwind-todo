e5cb628dcd006eb874e893a706a971b120ddb883
Tech Lord <45688942+techlord001@users.noreply.github.com>
2023-12-11 17:15:48 +0000

    Add: Completed Checkbox Functionality

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,8 +3,9 @@
         <h1 class="text-2xl font-bold">Todo List</h1>
         <input type="text" v-model="newTask" @keyup.enter="addTask" class="input" placeholder="Add a new task...">
         <ul>
-            <li v-for="task in tasks" :key="task.id">
+            <li v-for="task in tasks" :key="task.id" :class="{ 'completed': task.completed }">
                 {{ task.title }}
+                <input type="checkbox" v-model="task.completed">
             </li>
         </ul>
     </div>
@@ -16,6 +17,7 @@ import { defineComponent, ref } from 'vue'
 interface Task {
     id: number;
     title: string;
+    completed: boolean;
 }
 
 export default defineComponent({
@@ -28,12 +30,17 @@ export default defineComponent({
                 tasks.value.push({
                     id: Date.now(),
                     title: newTask.value,
+                    completed: false,
                 });
                 newTask.value = '';
             }
         };
 
-        return { tasks, newTask, addTask };
+        const toggleCompletion = (task: Task) => {
+            task.completed = !task.completed;
+        };
+
+        return { tasks, newTask, addTask, toggleCompletion };
     },
 });
 </script>
@@ -45,5 +52,9 @@ export default defineComponent({
 
 .input {
     @apply bg-gray-200 p-2 rounded mb-3 w-full;
+}
+
+.completed {
+    text-decoration: line-through;
 }
 </style>
